03b3b7ab477fe3f84177b7880fd4f8ee958a119a
totdy <tooltodiyo@gmail.com>
2024-12-13 21:29:11 +0000

    Day 1 done

---
+++ day_1/helloworld.py
@@ -0,0 +1,28 @@
+from math import sqrt
+
+print (3+4)
+print (3-4)
+print (3*4)
+print (3%4)
+print (3/4)
+print (3**4)
+print (3//4)
+
+print (type(10))
+print (type(9.8))
+print (type(3.14))
+print (type(4-4j))
+print (type(['Asabeneh','Python','Finland']))
+
+print (type(10))
+print (type(1.0))
+print (type(1j))
+print (type('Boba'))
+print (type(True))
+print (type([10,20,30]))
+print (type((10,20,30)))
+print (type({10,20,30}))
+print (type({'k1':10,'k2':20,'k3':30}))
+
+#Euclidian distance 
+print(sqrt((2-10)**2+(3-8)**2))
